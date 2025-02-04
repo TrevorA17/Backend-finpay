3fb6af3eebcc6fb3f0edcfac790e8176062eb498
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2025-02-04 16:42:16 +0300

    backend update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require("../models/User"); // Import User model
 
+//Get all users
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users", error });
+  }
+};
+
 // Add a new account
 const addUserAccount = async (req, res) => {
   const { userId } = req.params; // Get user ID from request params
@@ -57,4 +67,26 @@ const updateUserAccount = async (req, res) => {
   }
 };
 
-module.exports = { addUserAccount, updateUserAccount };
+//Get user accounts
+const getUserAccounts = async (req, res) => {
+  const { userId } = req.params; // Extract userId from request
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    // console.log("Sending user accounts:", user.userAccounts); //  Debug log
+
+    res.status(200).json(user.userAccounts); // Return user's accounts
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user accounts", error });
+  }
+};
+
+module.exports = {
+  addUserAccount,
+  updateUserAccount,
+  getAllUsers,
+  getUserAccounts,
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const {
   addUserAccount,
   updateUserAccount,
+  getAllUsers,
+  getUserAccounts,
 } = require("../controllers/userController");
 const router = express.Router();
 
@@ -11,9 +13,15 @@ router.post("/user/accounts/:userId", addUserAccount);
 // Route to update an existing user account
 router.put("/user/accounts/:userId/:accountId", updateUserAccount);
 
+//get all users
+router.get("/all", getAllUsers);
+
+//get user accounts
+router.get("/user/accounts/:userId", getUserAccounts);
+
 module.exports = router;
 
-// Postman URL for testing
+// Postman URL for testing user accounts
 // POST http://localhost:5000/api/user/accounts/userId
 // Content-Type: application/json
 
